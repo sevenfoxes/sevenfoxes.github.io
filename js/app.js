a636b55d0637694d18640170fa919f635580fcd4
Scott Nesham <scott.nesham@asynchrony.com>
2017-05-31 16:33:40 -0500

    recompile after toda's work

--- js/app.js
+++ js/app.js
@@ -31538,7 +31538,7 @@ module.exports = warning;
 fetchJobs = fetchJobs;var _isomorphicFetch = require('isomorphic-fetch');var _isomorphicFetch2 = _interopRequireDefault(_isomorphicFetch);var _actionTypes = require('../constants/actionTypes');var types = _interopRequireWildcard(_actionTypes);function _interopRequireWildcard(obj) {if (obj && obj.__esModule) {return obj;} else {var newObj = {};if (obj != null) {for (var key in obj) {if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key];}}newObj.default = obj;return newObj;}}function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };} // page loading actions
 function fetchJobsRequest() {return { type: types.FETCH_JOBS_REQUEST };}function fetchJobsSuccess(body) {return { type: types.FETCH_JOBS_SUCCESS, body: body };}function fetchJobsFailure(ex) {return { type: types.FETCH_JOBS_FAILURE, ex: ex };}function fetchJobs() {return function (dispatch) {
     dispatch(fetchJobsRequest());
-    return (0, _isomorphicFetch2.default)('https://jsonplaceholder.typicode.com/posts').
+    return (0, _isomorphicFetch2.default)('https://jsonplaceholder.typicode.com/users').
     then(function (response) {return response.json();}).
     then(function (json) {return dispatch(fetchJobsSuccess(json));}).
     catch(function (ex) {return dispatch(fetchJobsFailure(ex));});
@@ -31578,27 +31578,48 @@ var renderContent = function renderContent() {
 renderContent();
 
 },{"./components/ContentContainer":279,"./reducers/reducersIndex":282,"history/createBrowserHistory":30,"react":261,"react-dom":57,"react-hot-loader":183,"react-redux":219,"react-redux-toastr":206,"react-router":232,"redux":268,"redux-thunk":262}],279:[function(require,module,exports){
-'use strict';Object.defineProperty(exports, "__esModule", { value: true });var _createClass = function () {function defineProperties(target, props) {for (var i = 0; i < props.length; i++) {var descriptor = props[i];descriptor.enumerable = descriptor.enumerable || false;descriptor.configurable = true;if ("value" in descriptor) descriptor.writable = true;Object.defineProperty(target, descriptor.key, descriptor);}}return function (Constructor, protoProps, staticProps) {if (protoProps) defineProperties(Constructor.prototype, protoProps);if (staticProps) defineProperties(Constructor, staticProps);return Constructor;};}();var _react = require('react');var _react2 = _interopRequireDefault(_react);
+'use strict';Object.defineProperty(exports, "__esModule", { value: true });var _createClass = function () {function defineProperties(target, props) {for (var i = 0; i < props.length; i++) {var descriptor = props[i];descriptor.enumerable = descriptor.enumerable || false;descriptor.configurable = true;if ("value" in descriptor) descriptor.writable = true;Object.defineProperty(target, descriptor.key, descriptor);}}return function (Constructor, protoProps, staticProps) {if (protoProps) defineProperties(Constructor.prototype, protoProps);if (staticProps) defineProperties(Constructor, staticProps);return Constructor;};}();
+
+
+var _react = require('react');var _react2 = _interopRequireDefault(_react);
 var _reactRedux = require('react-redux');
 var _NavigationActions = require('../../actions/NavigationActions');function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}function _classCallCheck(instance, Constructor) {if (!(instance instanceof Constructor)) {throw new TypeError("Cannot call a class as a function");}}function _possibleConstructorReturn(self, call) {if (!self) {throw new ReferenceError("this hasn't been initialised - super() hasn't been called");}return call && (typeof call === "object" || typeof call === "function") ? call : self;}function _inherits(subClass, superClass) {if (typeof superClass !== "function" && superClass !== null) {throw new TypeError("Super expression must either be null or a function, not " + typeof superClass);}subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } });if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass;}var
+
 
 ContentContainer = function (_Component) {_inherits(ContentContainer, _Component);function ContentContainer() {_classCallCheck(this, ContentContainer);return _possibleConstructorReturn(this, (ContentContainer.__proto__ || Object.getPrototypeOf(ContentContainer)).apply(this, arguments));}_createClass(ContentContainer, [{ key: 'componentWillMount', value: function componentWillMount()
     {
       this.props.getJobs();
+    } }, { key: 'listJobs', value: function listJobs(
+
+    jobList) {
+      return this.props.jobs.map(function (job, i) {
+        return _react2.default.createElement('div', { key: i }, job.name);
+      });
     } }, { key: 'render', value: function render()
+
     {
-      console.log(this.props.jobs);
       return (
         _react2.default.createElement('div', null,
-          JSON.stringify(this.props.jobs)));
+          _react2.default.createElement('h1', null, 'People'),
+          _react2.default.createElement('div', null,
+            this.listJobs())));
+
 
 
     } }]);return ContentContainer;}(_react.Component);
-;exports.default =
-(0, _reactRedux.connect)(function (state) {return {
-    jobs: state.jobs };},
-function (dispatch) {return {
-    getJobs: function getJobs() {return dispatch((0, _NavigationActions.fetchJobs)());} };})(
+
+
+var mapStateToProps = function mapStateToProps(state) {return {
+    jobs: state.jobs };};
+
+
+var mapDispatchToProps = function mapDispatchToProps(dispatch) {return {
+    getJobs: function getJobs() {return dispatch((0, _NavigationActions.fetchJobs)());} };};exports.default =
+
+
+(0, _reactRedux.connect)(
+mapStateToProps,
+mapDispatchToProps)(
 ContentContainer);
 
 },{"../../actions/NavigationActions":277,"react":261,"react-redux":219}],280:[function(require,module,exports){
